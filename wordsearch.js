4305735ae6d0089d23f981b9498c34da5daec4bb
Raymond Lin <raymond.lin.7690@gmail.com>
2023-04-24 14:10:57 -0700

    implemented word-search function and added two mocha/chai tests

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    let column = "";
+    for (let j = 0; j < letters.length; j++) {
+      column += letters[j][i];
     }
-}
+    verticalJoin.push(column);
+  }
+  const allWords = horizontalJoin.concat(verticalJoin);
+  for (const words of allWords) {
+    if (words.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
